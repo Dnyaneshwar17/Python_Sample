779f381fe0122f76ee87a76632d18b6758227e46
Dnyaneshwar17 <42296046+Dnyaneshwar17@users.noreply.github.com>
2018-09-13 10:40:15 +0530

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,44 +1,19 @@
-
 import pytest
-import smtplib
 
-@pytest.fixture(scope="module")
-def smtp_connection():
-    return smtplib.SMTP("smtp.gmail.com", 587, timeout=5)
-	
-	@pytest.fixture(scope="session")
-def smtp_connection():
-    # the returned fixture value will be shared for
-    # all tests needing it
-    ...
-	
-#Higher-scoped fixtures are instantiated first¶	
-@pytest.fixture(scope="session")
-def s1():
-    pass
+@pytest.yield_fixture()
+def setUp():
+    print("Running method level setUp")
 
+@pytest.yield_fixture("class")
+def class_level():
+    print("class level setUp")
 
-@pytest.fixture(scope="module")
-def m1():
-    pass
+@pytest.yield_fixture("session")
+def session_level():
+    print("session level setUp")
+
+@pytest.yield_fixture("module")
+def module_level():
+    print("module level setUp")
 
 
-@pytest.fixture
-def f1(tmpdir):
-    pass
-
-
-@pytest.fixture
-def f2():
-    pass
-
-
-def test_foo(f1, m1, f2, s1):
-    ...	
-	
-	"""
-	s1: is the highest-scoped fixture (session).
-	m1: is the second highest-scoped fixture (module).
-	"""
-	
-	
