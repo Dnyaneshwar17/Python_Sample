23cb30047a73f61f466191f348346bd690b25248
Dnyaneshwar17 <42296046+Dnyaneshwar17@users.noreply.github.com>
2018-09-13 15:14:55 +0530

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,22 @@
 import pytest
+from base.selenium_driver import SeleniumDriver
 
 @pytest.yield_fixture()
 def setUp():
     print("Running method level setUp")
+    
+    
+@pytest.yield_fixture(scope="class")
+def one_time_browser_setup():
+    get_driver = SeleniumDriver(one_time_browser_setup)
+    driver = get_driver.select_browser()
+    return driver
 
-@pytest.yield_fixture("class")
-def class_level():
-    print("class level setUp")
-
-@pytest.yield_fixture("session")
+@pytest.yield_fixture(scope="session")
 def session_level():
     print("session level setUp")
 
-@pytest.yield_fixture("module")
+@pytest.yield_fixture(scope="module")
 def module_level():
     print("module level setUp")
 
