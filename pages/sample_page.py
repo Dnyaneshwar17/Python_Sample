24fe7b0ebd0339e904996e138f1b8f68580bb9aa
Dnyaneshwar17 <42296046+Dnyaneshwar17@users.noreply.github.com>
2018-09-13 15:19:11 +0530

    Update sample_page.py

--- pages/sample_page.py
+++ pages/sample_page.py
@@ -2,11 +2,10 @@ from base.basepage import BasePage
 
 
 class SampleDemo(BasePage):
-
-    # def __init__(self, value):
-    #     self.value = value
-
-    def __init__(self, driver):
+   
+    _page_valid = "//a[text()='LOG IN']"
+    
+   def __init__(self, driver):
         super().__init__(driver)
         self.driver = driver
 
@@ -17,5 +16,13 @@ class SampleDemo(BasePage):
     def string_valid(self, str1, str2):
         result = str2+str1
         return result
+    
+    
+    def page_valid(self):
+        driver = self.driver
+        result = self.is_element_present(locator=self._page_valid, locator_type="xpath")
+        driver.close()
+        return result
+
 
    
