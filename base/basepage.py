323407f5176b48bc1700df02af48eba4666e9cf1
Dnyaneshwar17 <42296046+Dnyaneshwar17@users.noreply.github.com>
2018-09-13 10:36:35 +0530

    Update basepage.py

--- base/basepage.py
+++ base/basepage.py
@@ -1,38 +1,12 @@
-""
-@package base
-
-Base Page class implementation
-It implements methods which are common to all the pages throughout the application
-
-This class needs to be inherited by all the page classes
-This should not be used by creating object instances
-
-Example:
-    Class LoginPage(BasePage)
-"""
-
 from base.selenium_driver import SeleniumDriver
-from traceback import print_stack
-from utilities.util import Util
 
 class BasePage(SeleniumDriver):
 
     def __init__(self, driver):
         """
         Inits BasePage class
-
         Returns:
-            None
+        None
         """
         super(BasePage, self).__init__(driver)
         self.driver = driver
-        self.util = Util()
-		
-    def verifyTitle(self, titleToVerify):
-        """
-        Verify the page Title
-
-        Parameters:
-            titleToVerify: Title on the page that needs to be verified
-        """		
-		
